af90ac5c799321b157612a971fcfbc927ea887a0
L1nar <122251664+L1nar@users.noreply.github.com>
2024-05-12 15:44:35 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,9 +5,13 @@ const result = document.getElementById('result');
 // Перед отправкой на сервер, этот объект нужно подготовить
 // Для этого вызывается метод open(), с двумя аргументами: метод протокола HTTP и путь к файлу на сервере, который нужно загрузить
 // Теперь запрос можно отправить с помощью метода send()
+// xhr.responseType = 'json'; - указываем объекту xhr, что ответ от сервера ожидается в json формате
+// Указать тип необходимо до отправки запроса методом send();
+// В свойстве response содержатся данные указанного типа
 btn.addEventListener('click', () => {
     const xhr = new XMLHttpRequest;
     xhr.open('GET', 'friends.json');
+    xhr.responseType = 'json'; // получить данные в формате json
     xhr.send();
 
     xhr.addEventListener('load', () => {
@@ -15,12 +19,13 @@ btn.addEventListener('click', () => {
             console.log('Something went wrong');
             result.textContent = 'Something went wrong';
         } else {    
-            const friends = JSON.parse(xhr.responseText);
+            result.innerHTML = '';
+            const friends = xhr.response; // мы указали тип получаемых данных в формате json, поэтому JSON.parse() не нужен
             console.log(friends);
             friends.forEach((friend) => {
                 result.innerHTML += friend.name;
                 result.innerHTML += friend.lastname;
             })
-        }
+        }   
     })
 })
